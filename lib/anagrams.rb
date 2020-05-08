a1b51b075f61783b45c26ac897b94e1f51f9866c
Ryan Duff <ryanduff613@gmail.com>
2020-05-08 13:45:07 -0700

    create first spec for instantiating a new object from class, achieve meaningful failure

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,2 +1,20 @@
 require ('rspec')
 
+
+# class Anagrams
+#   attr_reader(:user_entry1, :user_entry2)
+
+#   def initialize (user_entry1, user_entry2)
+#     @user_entry1 = user_entry1
+#     @user_entry2 = user_entry2
+#   end
+
+#   # def entry_length 
+#   #   if user_entry1.length() === user_entry2.length()
+#   #     is_anagram()
+#   #   else
+#   #     puts "in order to be anagrams, two words must be the same length. Please enter two words of the same length and try again"
+#   #     new_game = Anagrams.new()
+#   #   end
+
+# end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,3 +1,10 @@
 require ('pry')
 require ('anagrams')
 
+describe("Anagrams") do
+  it("creates a new instance of its class, takes two user inputs as variables and stores their values in instance variables")do
+    anagram = Anagrams.new("eat", "at")
+    expect(anagram.user_entry1).to(eq("eat"))
+  end
+
+end
