42e27e80de4f9b928e63a88498e33c82630f01a8
Ryan Duff <ryanduff613@gmail.com>
2020-05-08 13:54:03 -0700

    write second spec to test entry_length method. achieve meaningful failure

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -6,5 +6,10 @@ describe("Anagrams") do
     anagram = Anagrams.new("eat", "at")
     expect(anagram.user_entry1).to(eq("eat"))
   end
+end
 
+describe("Anagram#entry_length") do
+  it("checks length of user entries to make sure they are equal")
+    anagram = Anagrams.new("eat", "at")
+    expect(anagram.entry_length).to(eq(false))
 end
