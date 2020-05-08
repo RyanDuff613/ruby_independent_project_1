1b70503934ab05b331ab17f0027c0d803690c88d
Ryan Duff <ryanduff613@gmail.com>
2020-05-08 13:15:29 -0700

    add simple script file to get user input and call back-end method

--- anagrams_script.rb
+++ anagrams_script.rb
@@ -2,3 +2,9 @@
 
 require ('./lib/anagrams.rb')
 
+puts "please enter two words and I'll tell you if they are anagrams of one another"
+user_entry1 = gets.chomp
+puts "got that! now enter your second word"
+user_entry2 = gets.chomp
+
+anagrams = Anagrams.new(user_entry1, user_entry2)
