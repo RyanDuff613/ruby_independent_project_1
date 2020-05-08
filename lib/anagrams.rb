c9d9f092274f983bfbf6b58bf249b4aa77f504a3
Ryan Duff <ryanduff613@gmail.com>
2020-05-08 14:57:59 -0700

    create is_word method to evaluate if entry contains vowel, code working, tests passing

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,21 +1,34 @@
 require ('rspec')
-
+require ('pry')
 
 class Anagrams
   attr_reader(:user_entry1, :user_entry2)
+  
 
   def initialize (user_entry1, user_entry2)
-    @user_entry1 = user_entry1
-    @user_entry2 = user_entry2
+    @user_entry1 = user_entry1.downcase()
+    @user_entry2 = user_entry2.downcase()
   end
-
-  def entry_length 
-    if user_entry1.length() === user_entry2.length()
-      is_anagram()
+  
+  def entry_length? 
+    if @user_entry1.length() === @user_entry2.length()
+      # is_anagram()
+      true
     else
       puts "in order to be anagrams, two words must be the same length. Please enter two words of the same length and try again"
       false
     end
   end
+  def is_word?(input)
+    # binding.pry
+    if (input.split('').include? "a") || (input.split('').includes? "e") || (input.split('').includes? "i") || (input.split('').includes? "o") || (input.split('').includes? "u") || (input.split('').includes? "y")
+      true
+    else
+      false
+    end
+  end
+
+  # def is_anagram?
+  # end
 
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -10,7 +10,14 @@ end
 
 describe("Anagram#entry_length") do
   it("checks length of user entries to make sure they are equal")do
-    anagram = Anagrams.new("eat", "at")
-    expect(anagram.entry_length).to(eq(false))
+    anagram = Anagrams.new("eat", "ate")
+    expect(anagram.entry_length?()).to(eq(true))
+  end
+end
+
+describe("Anagram#is_word")do
+  it("check to make sure user entries both contain a vowel") do
+    anagram = Anagrams.new("eat", "ate")
+    expect(anagram.is_word?(anagram.user_entry1)).to(eq(true))
   end
 end
